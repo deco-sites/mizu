0022de56a835747519145d48b9dc7e2344cf9298
jovenan <jovenan2001@gmail.com>
2024-07-20 16:49:24 -0300

    feat: add cardsTrio styles

--- sections/Content/CardsTrio.tsx
+++ sections/Content/CardsTrio.tsx
@@ -1,59 +1,68 @@
-import type { HTMLWidget, VideoWidget } from "apps/admin/widgets.ts";
+import type { ImageWidget } from "apps/admin/widgets.ts";
+import Icon, { AvailableIcons } from "../../components/ui/Icon.tsx";
+import Image from "apps/website/components/Image.tsx";
+
+export interface CTA {
+    href: string;
+    text: string;
+    iconOnHover: AvailableIcons;
+}
+
+export interface Card {
+    image: ImageWidget;
+    href: string;
+    title?: string;
+    description?: string;
+    cta: CTA;
+}
 
 export interface Props {
-  title?: HTMLWidget;
-  video: {
-    src: VideoWidget;
-  };
+  cards: Card[];
 }
 
 export default function CardsTrio({
-  title,
-  video,
+    cards
 }: Props) {
   return (
-    <div class="relative w-full h-full">
-      {title &&
-        (
-          <div
-            class="absolute top-0 left-0 w-full h-full flex justify-center items-center"
-            style={{ background: "rgba(0,0,0,.64)" }}
-          >
-            <div class="w-full h-full lg:max-w-[1240px] flex items-center">
-              <h1
-                class="w-full font-black	text-white text-[40px] lg:text-[72px] leading-[40px] lg:leading-[64px] text-center lg:text-left mx-[31px] lg:mx-0"
-                dangerouslySetInnerHTML={{
-                  __html: title,
-                }}
-              />
+    <div class="px-[12px] lg:px-[40px] pt-[10px] lg:pt-[80px] pb-[50px] overflow-auto w-full flex gap-[4px] lg:gap-[24px] max-w-[1920px]">
+      {
+        cards.map(card => (
+            <div class="min-w-[318px]">
+                <a href={card.href} class="min-w-[318px] lg:w-full">
+                    <Image
+                        width={318}
+                        height={295}
+                        class="min-w-[318px] lg:w-full"
+                        src={card.image}
+                        alt={card.image}
+                        decoding="async"
+                        loading="lazy"
+                    />
+                </a>
+                <div>
+                    {
+                        card.title &&
+                        <h3 class="font-bold text-[40px] leading-[46.88px] text-[#060606] uppercase pt-[24px] pb-[16px]">{card.title}</h3>
+                    }
+                    {
+                        card.description &&
+                        <p class="text-[16px] leading-[18.75px] text-[#060606]">{card.description}</p>
+                    }
+                    <a
+                        class="w-min group relative pr-[90px] lg:pr-[40px] hover:pr-[90px] text-nowrap flex transition-all duration-300 justify-center items-center gap-10 bg-primary text-white py-[15px] px-[40px] mt-[24px] rounded font-roboto font-medium mt-[-20px]"
+                        href={card.cta.href}
+                    >
+                    {card.cta.text}
+                    <Icon
+                        class="lg:opacity-0 group-hover:opacity-100 absolute right-[40px] transition-all duration-300"
+                        id={card.cta.iconOnHover}
+                        size={18}
+                    />
+                    </a>
+                </div>
             </div>
-          </div>
-        )}
-      {video.src.includes("youtube")
-        ? (
-          <div class="h-[52.5vw] lg:h-[768px] px-[12px] lg:px-[40px] w-full bg-transparent lg:bg-[#d9d9d9] flex justify-center">
-            <iframe
-              class="max-w-[1366px]"
-              width="100%"
-              height="100%"
-              src={video.src}
-              frameborder="0"
-              allowFullScreen
-              allow="autoplay"
-            />
-          </div>
-        )
-        : (
-          <video
-            loop
-            autoplay
-            muted
-            playsinline
-            class="w-full h-[502px] lg:h-full object-cover"
-          >
-            <source src={video.src}></source>
-          </video>
-        )}
+        ))
+      }
     </div>
   );
 }
