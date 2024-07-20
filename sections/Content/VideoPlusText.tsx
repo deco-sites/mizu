d456a771794ee0e4d98f9f0555013b01fc9befd2
jovenan <jovenan2001@gmail.com>
2024-07-20 12:15:19 -0300

    fix: lint

--- sections/Content/VideoPlusText.tsx
+++ sections/Content/VideoPlusText.tsx
@@ -1,29 +1,28 @@
 import type { HTMLWidget, VideoWidget } from "apps/admin/widgets.ts";
 
-
 export interface Props {
   title?: HTMLWidget;
   video: {
-    src: VideoWidget
+    src: VideoWidget;
     marginOnX: boolean;
   };
 }
 
 export default function VideoPlusText({
   title,
-  video
+  video,
 }: Props) {
   return (
     <div class="relative w-full h-full">
-      {
-        title &&
-        <h1
-          class="font-black	text-white text-[40px] leading-[40px] text-center mx-[31px]"
-          dangerouslySetInnerHTML={{
-            __html: title,
-          }}
-        />
-      }
+      {title &&
+        (
+          <h1
+            class="font-black	text-white text-[40px] leading-[40px] text-center mx-[31px]"
+            dangerouslySetInnerHTML={{
+              __html: title,
+            }}
+          />
+        )}
       <video loop autoplay muted playsinline class="w-full h-full">
         <source src={video.src}></source>
       </video>
