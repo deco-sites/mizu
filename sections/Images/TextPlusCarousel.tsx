bafb897274121dff6791a3a0f8fa4d1ae8256e25
jovenan <jovenan2001@gmail.com>
2024-07-21 17:36:40 -0300

    feat: add textPlusCarousel style section

--- sections/Images/TextPlusCarousel.tsx
+++ sections/Images/TextPlusCarousel.tsx
@@ -1,10 +1,15 @@
 import { clx } from "../../sdk/clx.ts";
-import Icon from "../../components/ui/Icon.tsx";
+import Icon, { AvailableIcons } from "../../components/ui/Icon.tsx";
 import Slider from "../../components/ui/Slider.tsx";
 import { useId } from "../../sdk/useId.ts";
-import { ImageWidget } from "apps/admin/widgets.ts";
+import { HTMLWidget, ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 
+export interface CTA {
+    href: string;
+    text: string;
+    iconOnHover: AvailableIcons;
+}
 export interface CarouselImage {
   /** @description desktop otimized image */
   desktop: ImageWidget;
@@ -16,37 +21,62 @@ export interface CarouselImage {
   alt: string;
 }
 
+export interface Text {
+    title: HTMLWidget;
+    description: HTMLWidget;
+}
+
 interface Props {
+  summaries: Text[];
+  cta: CTA;
   carouselImages: CarouselImage[];
 }
 
-function TextPlusCarousel({ carouselImages }: Props) {
+function TextPlusCarousel({ carouselImages, cta, summaries }: Props) {
   const id = useId();
 
   return (
-    <>
+    <div class="flex flex-col lg:flex-row lg:justify-end gap-[24px] mt-[24px] lg:mt-[80px]">
+      <div class="flex flex-col gap-[40px] px-[12px] max-w-[357px]">
+        {
+            summaries.map(summary => (
+                <div class="flex flex-col gap-[24px]">
+                    <h3 class="text-primary font-black uppercase text-[32px] lg:text-[40px] leading-[37.5px]" dangerouslySetInnerHTML={{ __html: summary.title }} />
+                    <p class="font-roboto text-[16px] leading-[18.75px]" dangerouslySetInnerHTML={{ __html: summary.description }} />
+                </div>
+            ))
+        }
+        <a
+          class="mx-auto lg:mx-0 w-min group relative pr-[90px] lg:pr-[40px] hover:pr-[90px] text-nowrap flex transition-all duration-300 justify-center items-center gap-10 bg-primary text-white py-[15px] px-[40px] mt-[24px] rounded font-roboto font-medium mt-[-20px]"
+          href={cta.href}
+        >
+          {cta.text}
+          <Icon
+            class="lg:opacity-0 group-hover:opacity-100 absolute right-[40px] transition-all duration-300"
+            id={cta.iconOnHover}
+            size={18}
+          />
+        </a>
+      </div>
       <div
         id={id}
-        class="grid grid-rows-1"
-        style={{
-          gridTemplateColumns: "min-content 1fr min-content",
-        }}
+        class="max-w-[1180px] lg:w-[61%]"
       >
         <div class="col-start-1 col-span-3 row-start-1 row-span-1">
-          <Slider class="carousel carousel-center sm:carousel-end gap-5 sm:gap-10 w-full">
+          <Slider class="carousel carousel-left w-full transition-all">
             {carouselImages?.map((image, index) => (
               <Slider.Item
                 index={index}
                 class={clx(
-                  "carousel-item",
-                  "first:pl-5 first:sm:pl-0",
-                  "last:pr-5 last:sm:pr-0",
+                  "carousel-item px-[8px]",
+                  "first:pl-[8px]",
+                  "last:pr-[8px]",
                 )}
               >
                 <Image
-                  width={318}
-                  height={295}
-                  class="w-[287px] sm:w-[300px]"
+                  width={260}
+                  height={374}
+                  class="w-[260px] lg:w-[394px]"
                   src={image.desktop}
                   alt={image.desktop}
                   decoding="async"
@@ -57,20 +87,23 @@ function TextPlusCarousel({ carouselImages }: Props) {
           </Slider>
         </div>
 
-        <div class="col-start-1 col-span-1 row-start-1 row-span-1 z-10 self-center">
-          <Slider.PrevButton class="hidden sm:flex disabled:invisible btn btn-neutral btn-sm btn-circle no-animation">
-            <Icon id="chevron-right" class="rotate-180" />
-          </Slider.PrevButton>
+        <div class="flex w-full gap-[12px] justify-center items-center my-[20px]">
+            <div class="group">
+            <Slider.PrevButton class="w-[42px] h-[42px] flex justify-center items-center rounded-full border border-primary text-primary group-hover:text-white bg-transparent group-hover:bg-primary no-animation">
+                <Icon id="chevron-right" size={16} class="fill-[#001489] group-hover:fill-[#fff] rotate-180" />
+            </Slider.PrevButton>
+            </div>
+
+            <div class="group">
+            <Slider.NextButton class="w-[42px] h-[42px] flex justify-center items-center rounded-full border border-primary text-primary group-hover:text-white bg-transparent group-hover:bg-primary no-animation">
+                <Icon id="chevron-right" size={16} class="fill-[#001489] group-hover:fill-[#fff]"/>
+            </Slider.NextButton>
+            </div>
         </div>
 
-        <div class="col-start-3 col-span-1 row-start-1 row-span-1 z-10 self-center">
-          <Slider.NextButton class="hidden sm:flex disabled:invisible btn btn-neutral btn-sm btn-circle no-animation">
-            <Icon id="chevron-right" />
-          </Slider.NextButton>
-        </div>
       </div>
-      <Slider.JS rootId={id} />
-    </>
+      <Slider.JS rootId={id} scroll="smooth" />
+    </div>
   );
 }
 
