fafa4f44fb319c91503b35ccae0015a75072acd0
jovenan <jovenan2001@gmail.com>
2024-08-03 18:50:32 -0300

    feat: hero styles and section

--- sections/Content/Hero.tsx
+++ sections/Content/Hero.tsx
@@ -1,10 +1,12 @@
 import type { HTMLWidget, ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
+import Icon, { AvailableIcons } from "../../components/ui/Icon.tsx";
 
 export interface CTA {
   id?: string;
   href: string;
   text: string;
+  iconOnHover: AvailableIcons;
   variant: "Normal" | "Reverse";
 }
 
@@ -13,93 +15,89 @@ export interface Props {
   description?: string;
   image?: ImageWidget;
   mobileImage?: ImageWidget;
+  imageURL?: string;
   placement?: "left" | "right" | "center";
-  cta: CTA[];
+  cta?: CTA[];
 }
 
 const PLACEMENT = {
-  left: "flex-col text-left lg:flex-row-reverse",
-  right: "flex-col text-left lg:flex-row",
-  center: "flex-col text-center lg:flex-row",
+  left: "flex-col items-start text-left justify-center lg:items-center",
+  right: "flex-col items-end text-left justify-center lg:items-center",
+  center: "flex-col items-center justify-center text-center",
 };
 
 export default function HeroFlats({
-  title = "Hero",
-  description = "Your description here",
+  title,
+  description,
   image,
   mobileImage,
-  placement = "left",
+  placement = "center",
+  imageURL = "#",
   cta = [],
 }: Props) {
   return (
     <div>
-      <div class="mx-auto flex flex-col items-center gap-8">
+      <div class="mx-auto flex flex-col items-center lg:pt-[30px] mb-[30px] lg:mb-[94px]">
         <div
-          class={`flex w-full xl:container xl:mx-auto py-20 mx-5 md:mx-10 z-10 ${
-            image
-              ? PLACEMENT[placement]
-              : "flex-col items-center justify-center text-center"
-          } lg:py-36 gap-12 md:gap-20 items-center`}
+          class={`flex w-full relative items-center`}
         >
-          {image && (
-            <Image
-              width={640}
-              class="w-full lg:w-1/2 object-fit"
-              sizes="(max-width: 640px) 100vw, 30vw"
-              src={image}
-              alt={image}
-              decoding="async"
-              loading="lazy"
-            />
-          )}
-          {mobileImage && (
-            <Image
-              width={640}
-              class="w-full lg:w-1/2 object-fit"
-              sizes="(max-width: 640px) 100vw, 30vw"
-              src={mobileImage}
-              alt={mobileImage}
-              decoding="async"
-              loading="lazy"
-            />
-          )}
+          <a href={imageURL} class="w-full">
+            {image && (
+              <Image
+                width={640}
+                class="hidden lg:flex w-full object-fit"
+                sizes="(max-width: 640px) 100vw, 30vw"
+                src={image}
+                alt={image}
+                decoding="async"
+                loading="lazy"
+              />
+            )}
+            {mobileImage && (
+              <Image
+                width={640}
+                class="w-full lg:hidden object-fit"
+                sizes="(max-width: 640px) 100vw, 30vw"
+                src={mobileImage}
+                alt={mobileImage}
+                decoding="async"
+                loading="lazy"
+              />
+            )}
+          </a>
           <div
-            class={`mx-6 lg:mx-auto lg:w-full space-y-4 gap-4 ${
-              image
-                ? "lg:w-1/2 lg:max-w-xl"
-                : "flex flex-col items-center justify-center lg:max-w-3xl"
-            }`}
+            class={`absolute top-[45px] left-0 w-full h-full`}
           >
             <div
-              class="inline-block text-[80px] leading-[100%] font-medium tracking-[-2.4px]"
-              dangerouslySetInnerHTML={{
-                __html: title,
-              }}
+              class={`flex max-w-[1250px] mx-auto w-full h-full ${
+                PLACEMENT[placement]
+              }`}
             >
-            </div>
-            <p class="text-zinc-400 text-[16px] md:text-[18px] leading-[150%]">
-              {description}
-            </p>
-            <div class="flex flex-col items-center lg:items-start lg:flex-row gap-4">
-              {cta?.map((item) => (
-                <a
-                  key={item?.id}
-                  id={item?.id}
-                  href={item?.href}
-                  target={item?.href.includes("http") ? "_blank" : "_self"}
-                  class={`group relative overflow-hidden rounded-full hover:bg-gradient-to-r px-6 py-2 lg:px-8 lg:py-3 transition-all duration-300 ease-out ${
-                    item.variant === "Reverse"
-                      ? "bg-secondary text-white"
-                      : "bg-accent text-black"
-                  }`}
-                >
-                  <span class="ease absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 transform bg-white opacity-10 transition-all duration-1000 group-hover:-translate-x-40">
-                  </span>
-                  <span class="relative font-medium lg:text-[20px]">
-                    {item?.text}
-                  </span>
-                </a>
-              ))}
+              <div
+                class="text-[#F2F2F2] uppercase tracking-[0px] font-bold text-[32px] font-roboto"
+                dangerouslySetInnerHTML={{
+                  __html: title || "",
+                }}
+              >
+              </div>
+              <p class="text-[#F2F2F2] tracking-[0px] text-[16px] lg:text-[20px] font-roboto">
+                {description}
+              </p>
+              <div class="flex flex-col items-center lg:items-start lg:flex-row gap-4">
+                {cta?.map((item) => (
+                  <a
+                    class="w-min group relative pr-[90px] lg:pr-[40px] hover:pr-[90px] text-nowrap flex transition-all duration-300 justify-center items-center gap-10 bg-[#F2F2F2] text-[#000] py-[15px] px-[40px] mt-[24px] rounded-[28px] font-roboto font-medium mt-[-20px]"
+                    href={item.href}
+                  >
+                    {item.text}
+                    <Icon
+                      class="lg:opacity-0 group-hover:opacity-100 absolute right-[40px] transition-all duration-300"
+                      id={item.iconOnHover}
+                      size={18}
+                    />
+                  </a>
+                ))}
+              </div>
             </div>
           </div>
         </div>
